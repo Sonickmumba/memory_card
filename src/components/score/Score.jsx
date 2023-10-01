5771f2bfa6a53e38060ad969adbcbe1875adb994
Sonick Mumba <kingellie.mumba@gmail.com>
2023-10-02 01:51:24 +0200

    Update Score.jsx

--- src/components/score/Score.jsx
+++ src/components/score/Score.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Score = () => {
   return (
-    <div>Score</div>
+    <div className="scores-div">
+      <div className="score-best">
+        <p>Scores: 3</p>
+        <p>Best scores: 9</p>
+      </div>
+    </div>
   )
 }
 
