8571581690bd87ea2d05952dba867e4d3a5cbad9
Sonick Mumba <kingellie.mumba@gmail.com>
2023-10-01 23:57:45 +0200

    Update App.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Header from './components/header/Header';
+import Score from './components/score/Score';
+import Game from './components/game/Game';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
+        <Header />
+        <Score />
+        <Game />
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,9 +20,6 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
